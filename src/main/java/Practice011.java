0e6d07af226497c7588eab984ecdc2d58182d6ac
Yoonseojun <bepatient1@naver.com>
2022-03-18 19:57:04 +0900

    check

---
+++ src/main/java/Practice011.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practice011 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int i = in.nextInt();
+
+        for(int a = 0; a < i; a++){
+
+            int n1 = in.nextInt();
+            int n2 = in.nextInt();
+
+            System.out.println(n1+n2);
+
+        }
+
+    }
+
+}
