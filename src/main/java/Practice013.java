590687605d321c9de19fe53ecd6f9e88c00df93f
Yoonseojun <bepatient1@naver.com>
2022-03-19 23:03:41 +0900

    check

---
+++ src/main/java/Practice013.java
@@ -0,0 +1,11 @@
+public class Practice013 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=20; i++){
+            System.out.println((int)(Math.random()*11)-5);
+        }
+
+    }
+
+}
