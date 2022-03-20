d45b746a8c2c48cf185a2315a6f3479df5c1b7ae
Yoonseojun <bepatient1@naver.com>
2022-03-20 19:14:23 +0900

    check

---
+++ src/main/java/P1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class P1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = b/100;
+        int y = (b % 100)/10;
+        int z = b % 10;
+
+        System.out.println(a*z);
+        System.out.println(a*y);
+        System.out.println(a*x);
+        System.out.println(a*b);
+
+    }
+
+}
