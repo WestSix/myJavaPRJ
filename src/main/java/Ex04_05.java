4f2acbe79b3ce747847fc7032bb8f6956c748314
Yoonseojun <bepatient1@naver.com>
2022-03-23 11:36:13 +0900

    2022.03.23 Commit - 변수, 연산자

---
+++ src/main/java/Ex04_05.java
@@ -0,0 +1,22 @@
+public class Ex04_05 {
+
+    public static void main(String[] args) {
+
+        int a = 100, b = 200;
+
+        /*관계 연산자*/
+        System.out.printf("%d == %d 는 %s 이다.\n", a, b, a == b);
+        System.out.printf("%d != %d 는 %s 이다.\n", a, b, a != b);
+        System.out.printf("%d > %d 는 %s 이다.\n", a, b, a > b);
+        System.out.printf("%d < %d 는 %s 이다.\n", a, b, a < b);
+        System.out.printf("%d >= %d 는 %s 이다.\n", a, b, a >= b);
+        System.out.printf("%d <= %d 는 %s 이다.\n", a, b, a <= b);
+        /*관계 연산자*/
+
+        /*대입 연산자*/
+        System.out.printf("%d = %d 는 %s 이다.\n", a, b, a = b);
+        /*대입 연산자*/
+
+    }
+
+}
