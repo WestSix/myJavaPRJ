208c6890618dc22045ff53a7b8d4d7399e2b0b45
Yoonseojun <bepatient1@naver.com>
2022-03-30 10:13:45 +0900

    2022.03.30 Commit - if

--- src/main/java/Ex05_01.java
+++ src/main/java/Ex05_01.java
@@ -2,9 +2,12 @@ public class Ex05_01 {
 
     public static void main(String[] args) {
 
-        int a = 99;
+        int a = 199;
         if(a<100)
             System.out.println("100보다 작군요 \n");
+            System.out.println("거짓이므로 이 문장은 안보이겠죠?");
+
+        System.out.println("프로그램 끝");
 
     }
 
---
+++ src/main/java/Ex05_02.java
@@ -0,0 +1,14 @@
+public class Ex05_02 {
+
+    public static void main(String[] args) {
+
+        int a = 200;
+        if(a < 100){
+            System.out.println("100보다 작군요");
+        }else{
+            System.out.println("100보다 크군요");
+        }
+
+    }
+
+}
