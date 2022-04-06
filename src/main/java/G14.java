2ae5712a91785e0d0e78cbd1cc8cf8a263471593
Yoonseojun <bepatient1@naver.com>
2022-04-06 10:24:47 +0900

    2022.04.06 Commit - for

---
+++ src/main/java/G14.java
@@ -0,0 +1,11 @@
+public class G14 {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("for 문을 공부중입니다.");
+        }
+
+    }
+
+}
