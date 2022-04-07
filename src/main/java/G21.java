ba5b8407b5940f76aa602f7e12c744339af71212
Yoonseojun <bepatient1@naver.com>
2022-04-08 08:55:42 +0900

    2022.04.08 Commit - for

---
+++ src/main/java/G21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class G21 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+
+        for(int j = 1; j <=i; j++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a+b);
+        }
+
+    }
+
+}
