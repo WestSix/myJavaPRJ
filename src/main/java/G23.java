0a2c2af14382be674a9cba8abbde1614a5f40e96
Yoonseojun <bepatient1@naver.com>
2022-04-12 09:30:16 +0900

    2022.04.12 Commit - for

---
+++ src/main/java/G23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class G23 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = 0;
+
+        for(i = 1; i <= a; i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
