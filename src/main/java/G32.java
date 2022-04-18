7cf50ac08ab8a182097f3e93b7384a093e2b7678
Yoonseojun <bepatient1@naver.com>
2022-04-18 15:06:21 +0900

    2022.04.18 Commit - for

---
+++ src/main/java/G32.java
@@ -0,0 +1,19 @@
+public class G32 {
+
+    public static void main(String[] args) {
+
+        int a, b;
+
+        for(a = 0; a < 3; a++){
+
+            for(b = 0; b < 3; b++){
+
+                System.out.println("a = " + a + ", b = " + b);
+
+            }
+
+        }
+
+    }
+
+}
