c4b1bade4f626b3bd4e31254b1ec7b11ca1d481d
Yoonseojun <bepatient1@naver.com>
2022-04-19 17:33:51 +0900

    2022.04.19 Commit - for

---
+++ src/main/java/G38.java
@@ -0,0 +1,18 @@
+public class G38 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int a = 1;
+
+        while(a <= 10){
+            sum += a;
+            a++;
+        }
+
+        System.out.println("1부터 10까지의 합 = " + sum);
+
+
+    }
+
+}
