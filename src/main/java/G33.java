a55474d2988e93c8e82094e8893fa6717673d025
Yoonseojun <bepatient1@naver.com>
2022-04-19 09:01:00 +0900

    2022.04.19 Commit - for

---
+++ src/main/java/G33.java
@@ -0,0 +1,22 @@
+public class G33 {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 1;
+
+        for(a = 2; a < 10; a++){
+
+            for(b = 1; b < 10; b++){
+
+                System.out.println(a + " * " + b + " = " + (a*b));
+
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+}
