78cd7f28b4a9881acade3eb59b8c974aca914fad
Yoonseojun <bepatient1@naver.com>
2022-04-19 09:03:04 +0900

    2022.04.19 Commit - for

---
+++ src/main/java/G37.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class G37 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int i;
+        int j;
+
+        for(i = 1; i <= a; i++){
+
+            for(j = a; j > 0; j--){
+
+                if(i < j){
+
+                    System.out.print(" ");
+
+                }else{
+
+                    System.out.print("*");
+
+                }
+
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+}
