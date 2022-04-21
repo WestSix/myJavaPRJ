7a285fbfc78b9dfdd597f207bb96dfd0c2c72474
Yoonseojun <bepatient1@naver.com>
2022-04-22 08:35:27 +0900

    2022.04.22 Commit - for

---
+++ src/main/java/G43.java
@@ -0,0 +1,17 @@
+public class G43 {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        for(i = 1; i <= 100; i++){
+            System.out.println("for 문을 " + i + " 회 실행했습니다.");
+            break;
+        }
+
+        System.out.println("");
+        System.out.println("for 문을 종료했습니다.");
+
+    }
+
+}
