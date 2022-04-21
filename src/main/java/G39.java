e07bec70ddeb6b469b78423c1e7974438d03d5d1
Yoonseojun <bepatient1@naver.com>
2022-04-22 08:34:49 +0900

    2022.04.22 Commit - for

---
+++ src/main/java/G39.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class G39 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int i = 0;
+        int sum = 0;
+
+        while(i <= a){
+
+            sum += i;
+            i++;
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
