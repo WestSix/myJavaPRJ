10297c1632b5a0e77c2bda851f9705c40d457954
Yoonseojun <bepatient1@naver.com>
2022-04-25 14:28:24 +0900

    2022.04.25 Commit - for

---
+++ src/main/java/G49.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class G49 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < a; i++){
+            for (j = a; j >= 0; j--){
+                if (i < j){
+                    System.out.print("");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+
+        for (i = a-1; i >= 1; i--){
+            for (j = i; j >= 1; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
