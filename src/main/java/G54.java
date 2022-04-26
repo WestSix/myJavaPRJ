e486f80f984490ed2f606b161250a472feda9f8c
Yoonseojun <bepatient1@naver.com>
2022-04-27 08:55:23 +0900

    2022.04.27 Commit - for

---
+++ src/main/java/G54.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class G54 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        int j=1;
+        int stop=sc.nextInt();
+        for(i=sc.nextInt();i<10;i++){
+            for(j=1;j<10;j++){System.out.println(i+" * "+j+" = "+(i*j));}
+            if(i==stop){break;}
+        }
+    }
+}
