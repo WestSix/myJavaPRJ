52a0f41475670405e32988e309268479c9f001f5
Yoonseojun <bepatient1@naver.com>
2022-04-29 14:08:33 +0900

    2022.04.29 Commit - for

---
+++ src/main/java/G57.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class G57 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a % 15 == 0){
+            System.out.println();
+        }
+
+    }
+
+}
