b254b1ac027082faee47d7522f9e17b03e03d608
Yoonseojun <bepatient1@naver.com>
2022-05-04 09:13:48 +0900

    2022.05.04 Commit - for

---
+++ src/main/java/G62.java
@@ -0,0 +1,16 @@
+public class G62 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int i = 50; i < 101; i++){
+            if(i % 3 != 0 || i % 7 != 0 || i % 11 != 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+
+    }
+
+}
