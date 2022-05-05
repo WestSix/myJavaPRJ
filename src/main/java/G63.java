dc73fc4ec37ce6d7e098feff83e2e48b3d50c424
Yoonseojun <bepatient1@naver.com>
2022-05-05 23:12:33 +0900

    2022.05.04 Commit - for

--- src/main/java/G63.java
+++ src/main/java/G63.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
-
 public class G63 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        for(a = 3; a < 9; a++){
+            for(b = 2; b < 7; b++){
+                System.out.println(a + " * " + b + " = " + (a*b));
+            }
+        }
 
     }
 
