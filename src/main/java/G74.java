a9d0f27f3c987ae427d2816d147cd0bfaa5d8d89
Yoonseojun <bepatient1@naver.com>
2022-06-03 21:13:06 +0900

    2022.06.03 Commit - map

---
+++ src/main/java/G74.java
@@ -0,0 +1,15 @@
+public class G74 {
+
+    public static void main(String[] args) {
+
+        double a = 32.72;
+        double sum = 0;
+
+        for(int i = 0; i < 20; i++){
+            sum +=a;
+            System.out.println(sum);
+        }
+
+    }
+
+}
