8f5b4d66edbdddf5d83ec4bd360f2ad64c1715c6
Yoonseojun <bepatient1@naver.com>
2022-06-07 17:34:01 +0900

    2022.06.07 Commit - array

---
+++ src/main/java/G0518_1.java
@@ -0,0 +1,15 @@
+public class G0518_1 {
+
+    public static void main(String[] args) {
+
+        String str = "IT CookBook.java";
+        int len;
+
+        len = str.length();
+
+        System.out.printf("문자열 : %s\n", str);
+        System.out.printf("문자열 길이 : %d\n", len);
+
+    }
+
+}
