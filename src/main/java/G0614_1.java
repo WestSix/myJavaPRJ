b1e8ec8706ee048fe3169a033e245ff652480d61
Yoonseojun <bepatient1@naver.com>
2022-06-24 00:30:26 +0900

    2022.06.24 Commit - array

---
+++ src/main/java/G0614_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class G0614_1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[sc.nextInt()];
+
+    }
+
+}
